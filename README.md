2463e8e5d8303b2b43ec59aa10107165de5264b2
Frode Oijord <foijord@gmail.com>
2022-11-22 14:32:15 +0100

    added new reproducer for creating and deleting many large images

--- README.md
+++ README.md
@@ -14,10 +14,10 @@ Open a Developer PowerShell for Visual Studio 2022. Create a Build folder inside
 ```
 PS C:\code\VulkanImageSizeReproducer\Build> cmake.exe ..
 PS C:\code\VulkanImageSizeReproducer\Build> MSBuild.exe .\VulkanImageSizeReproducer.sln
-PS C:\code\VulkanImageSizeReproducer\Build> .\Debug\VulkanImageSizeReproducer.exe
+PS C:\code\VulkanImageSizeReproducer\Build> .\Debug\VulkanImageMaxSizeReproducer.exe
 ```
 
-Output on my system:
+Output from Laptop:
 ```
 Creating device: NVIDIA RTX A5000 Laptop GPU
 NVIDIA Driver version: 517.40.0.0
@@ -32,4 +32,40 @@ VkImageFormatPropeties.maxExtent = { 16384, 16384, 2048 }
 VkPhysicalDeviceFormatProperties2.properties.limits.maxImageDimension3D = 2048
 Creating image with extent: { 16384, 16384, 2048 }
 vkCreateImage returned VK_SUCCESS
+```
+Output from Desktop:
+```
+Creating device: Quadro RTX 8000
+NVIDIA Driver version: 526.67.0.0
+VkImageFormatPropeties.maxExtent = { 16384, 16384, 16384 }
+VkPhysicalDeviceFormatProperties2.properties.limits.maxImageDimension3D = 16384
+Creating image with extent: { 16384, 16384, 16384 }
+vkCreateImage returned VK_ERROR_OUT_OF_DEVICE_MEMORY
+```
+
+## Creating and deleting many large VkImages eventually fails ##
+When creating a VkImage of of extent 1024 x 1024 x 1024 and deleting it, vkCreateImage returns VK_ERROR_OUT_OF_DEVICE_MEMORY after many iterations.
+
+## Build ##
+### Prerequisites ###
+- Windows 10
+- Microsoft Visual Studio 2022 (64-bit) Version 17.3.2 or later with the "Desktop development with C++" Workload installed.
+- NVIDIA GPU, Pascal or more recent
+- NVIDIA graphics driver installed (version 526.67)
+- Vulkan SDK installed (1.3.231)
+
+Open a Developer PowerShell for Visual Studio 2022. Create a Build folder inside this repo, build and execute:
+
+```
+PS C:\code\VulkanImageSizeReproducer\Build> cmake.exe ..
+PS C:\code\VulkanImageSizeReproducer\Build> MSBuild.exe .\VulkanImageSizeReproducer.sln
+PS C:\code\VulkanImageSizeReproducer\Build> .\Debug\VulkanImageMemoryLeakReproducer.exe
+```
+
+Output from Desktop:
+```
+Creating device: Quadro RTX 8000
+NVIDIA Driver version: 526.67.0.0
+Creating image with extent: { 1024, 1024, 1024 }
+vkCreateImage returned VK_ERROR_OUT_OF_DEVICE_MEMORY after 883 iterations
 ```
---
+++ VulkanImageMemoryLeakReproducer.cpp
@@ -0,0 +1,193 @@
+
+#pragma once
+
+#define VOLK_IMPLEMENTATION
+#ifdef _WIN32
+#include <Volk/volk.h>
+#else
+#include <volk.h>
+#endif
+
+#include <map>
+#include <string>
+#include <vector>
+#include <iostream>
+
+
+int main(int, char* [])
+{
+	volkInitialize();
+
+	VkApplicationInfo applicationInfo{
+		.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+		.pNext = nullptr,
+		.pApplicationName = "Image Size Test",
+		.applicationVersion = 1,
+		.pEngineName = "Image Size Test",
+		.engineVersion = 1,
+		.apiVersion = VK_API_VERSION_1_0,
+	};
+
+	std::vector<const char*> instance_layers{
+		"VK_LAYER_KHRONOS_validation"
+	};
+	std::vector<const char*> instance_extensions{
+		VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME
+	};
+
+	VkInstanceCreateInfo instanceCreateInfo{
+		.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+		.pNext = nullptr,
+		.flags = 0,
+		.pApplicationInfo = &applicationInfo,
+		.enabledLayerCount = static_cast<uint32_t>(instance_layers.size()),
+		.ppEnabledLayerNames = instance_layers.data(),
+		.enabledExtensionCount = static_cast<uint32_t>(instance_extensions.size()),
+		.ppEnabledExtensionNames = instance_extensions.data()
+	};
+
+	VkInstance instance;
+	if (vkCreateInstance(&instanceCreateInfo, nullptr, &instance) != VK_SUCCESS) {
+		std::cerr << "vkCreateInstance failed!";
+		return EXIT_FAILURE;
+	}
+
+	volkLoadInstance(instance);
+
+	uint32_t physicalDeviceCount;
+	vkEnumeratePhysicalDevices(instance, &physicalDeviceCount, nullptr);
+
+	std::vector<VkPhysicalDevice> physicalDevices(physicalDeviceCount);
+	vkEnumeratePhysicalDevices(instance, &physicalDeviceCount, physicalDevices.data());
+
+	for (auto physicalDevice : physicalDevices) {
+
+		std::vector<const char*> device_layers{
+			"VK_LAYER_KHRONOS_validation"
+		};
+		std::vector<const char*> device_extensions{};
+
+		std::vector<float> queuePriorities = { 1.0f };
+
+		std::vector<VkDeviceQueueCreateInfo> deviceQueueCreateInfos{ {
+			.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+			.pNext = nullptr,
+			.flags = 0,
+			.queueFamilyIndex = 0,
+			.queueCount = static_cast<uint32_t>(queuePriorities.size()),
+			.pQueuePriorities = queuePriorities.data()
+		} };
+
+		VkPhysicalDeviceFeatures physicalDeviceFeatures{};
+		physicalDeviceFeatures.sparseBinding = VK_TRUE;
+		physicalDeviceFeatures.sparseResidencyImage3D = VK_TRUE;
+
+		VkDeviceCreateInfo deviceCreateInfo{
+			.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+			.pNext = nullptr,
+			.flags = 0,
+			.queueCreateInfoCount = static_cast<uint32_t>(deviceQueueCreateInfos.size()),
+			.pQueueCreateInfos = deviceQueueCreateInfos.data(),
+			.enabledLayerCount = static_cast<uint32_t>(device_layers.size()),
+			.ppEnabledLayerNames = device_layers.data(),
+			.enabledExtensionCount = static_cast<uint32_t>(device_extensions.size()),
+			.ppEnabledExtensionNames = device_extensions.data(),
+			.pEnabledFeatures = &physicalDeviceFeatures,
+		};
+
+		VkPhysicalDeviceProperties physicalDeviceProperties;
+		vkGetPhysicalDeviceProperties(physicalDevice, &physicalDeviceProperties);
+
+		std::cout << "Creating device: " << physicalDeviceProperties.deviceName << std::endl;
+
+		// NVIDIA version scheme
+		if (physicalDeviceProperties.vendorID == 4318) {
+			std::cout << "NVIDIA Driver version: " <<
+				((physicalDeviceProperties.driverVersion >> 22) & 0x3ff) << "." <<
+				((physicalDeviceProperties.driverVersion >> 14) & 0x0ff) << "." <<
+				((physicalDeviceProperties.driverVersion >> 6) & 0x0ff) << "." <<
+				((physicalDeviceProperties.driverVersion) & 0x003f) << std::endl;
+		}
+#ifdef _WIN32
+		// INTEL version scheme (only on Windows)
+		else if (physicalDeviceProperties.vendorID == 0x8086) {
+			std::cout << "Intel Driver version: " <<
+				(physicalDeviceProperties.driverVersion >> 14) << "." <<
+				(physicalDeviceProperties.driverVersion & 0x3ff) << std::endl;
+		}
+#endif
+		else {
+			// standard Vulkan versioning
+			std::cout << "Driver version: " <<
+				(physicalDeviceProperties.driverVersion >> 22) << "." <<
+				((physicalDeviceProperties.driverVersion >> 12) & 0x3ff) << "." <<
+				(physicalDeviceProperties.driverVersion & 0xfff) << std::endl;
+		}
+
+		VkDevice device;
+		if (vkCreateDevice(physicalDevice, &deviceCreateInfo, nullptr, &device) != VK_SUCCESS) {
+			std::cerr << "vkCreateDevice failed!";
+			return EXIT_FAILURE;
+		}
+
+		VkFormat format = VK_FORMAT_R8_UNORM;
+		VkImageType imageType = VK_IMAGE_TYPE_3D;
+		VkImageTiling tiling = VK_IMAGE_TILING_OPTIMAL;
+		VkImageUsageFlags usage = VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT;
+		VkImageCreateFlags flags = VK_IMAGE_CREATE_SPARSE_BINDING_BIT | VK_IMAGE_CREATE_SPARSE_RESIDENCY_BIT;
+
+		VkImageFormatProperties imageFormatProperties;
+		if (vkGetPhysicalDeviceImageFormatProperties(physicalDevice, format, imageType, tiling, usage, flags, &imageFormatProperties) != VK_SUCCESS) {
+			std::cerr << "vkGetPhysicalDeviceImageFormatProperties!";
+			return EXIT_FAILURE;
+		}
+
+		VkExtent3D extent{
+			std::min(1024u, imageFormatProperties.maxExtent.width),
+			std::min(1024u, imageFormatProperties.maxExtent.height),
+			std::min(1024u, imageFormatProperties.maxExtent.depth)
+		};
+
+		VkImageCreateInfo imageCreateInfo{
+			.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO,
+			.pNext = nullptr,
+			.flags = flags,
+			.imageType = imageType,
+			.format = format,
+			.extent = extent,
+			.mipLevels = 1,
+			.arrayLayers = 1,
+			.samples = VK_SAMPLE_COUNT_1_BIT,
+			.tiling = tiling,
+			.usage = usage,
+			.sharingMode = VK_SHARING_MODE_EXCLUSIVE,
+			.queueFamilyIndexCount = 0,
+			.pQueueFamilyIndices = nullptr,
+			.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+		};
+
+		std::cout << "Creating image with extent: { " <<
+			imageCreateInfo.extent.width << ", " <<
+			imageCreateInfo.extent.height << ", " <<
+			imageCreateInfo.extent.depth << " }" << std::endl;
+
+		for (size_t count = 0; count < 1000; count++) {
+			VkImage image;
+			auto result = vkCreateImage(device, &imageCreateInfo, nullptr, &image);
+			if (result == VK_SUCCESS) {
+				vkDestroyImage(device, image, nullptr);
+			}
+			else {
+				if (result == VK_ERROR_OUT_OF_DEVICE_MEMORY) {
+					std::cerr << "vkCreateImage returned VK_ERROR_OUT_OF_DEVICE_MEMORY" << " after " << count << " iterations" << std::endl;
+				}
+				else {
+					std::cerr << "vkCreateImage failed with result " << result << std::endl;
+				}
+				break;
+			}
+		}
+		std::cout << std::endl;
+	}
+	return EXIT_SUCCESS;
+}
